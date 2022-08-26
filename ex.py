5fa8b150b45dce52e434b23f81162757ae7a9add
Amirg2200 <112168029+Amirg2200@users.noreply.github.com>
2022-08-26 16:04:51 +0200

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,18 +1,17 @@
-#Skal afleveres til mtal@kea.dk
-#Exercise-1
+# Exercise-1
 
 word = '"It is your fault!" he said'
 
 print(word)
 
-#Exercise-2
+# Exercise-2
 
 word2 = "I'm fine"
 
 print(word2)
 
 
-#Exercise-3
+# Exercise-3
 
 sentence = """     How are you dear
      child? I hope you are doing good"""
@@ -21,33 +20,31 @@ sentence = """     How are you dear
 print(sentence)
 
 
-#Exercise-4
+# Exercise-4
 
 sentence2 = "How are you dear,\
  I hope you are doing good"
 
 print(sentence2)
 
-#Excercise-5
+# Excercise-5
 
-leng = 'Hello, how are you today?'
-print(leng)
-print(len(leng))
+length = "Hello, how are you today?"
+print(length)
+print(len(length))
 
-#Exercise-6
+# Exercise-6
 
-string1 = 'Nice to see you.'
-string2 = 'Nice to see you too buddy.'
+string1 = "Nice to see you."
+string2 = "Nice to see you too buddy."
 string3 = string1 + string2
 print(string3)
 
-#Exercise-7
+# Exercise-7
 
 string3 = string1 + " " + string2
 print(string3)
 
-#Exercise-8
+# Exercise-8
 
-print("bazinga")
 print("bazinga"[2:6])
-     
