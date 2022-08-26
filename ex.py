70ff926a56c424b98320a3c242e68c45111f977f
Amirg2200 <112168029+Amirg2200@users.noreply.github.com>
2022-08-26 12:12:19 +0200

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,28 @@
+#Exercise-1
+
+word = 'Hemiska hora! "sagdi hann"'
+
+print(word)
+
+#Exercise-2
+
+word2 = "I'm fine"
+
+print(word2)
+
+
+#Exercise-3
+
+sentence = """     How are you dear
+                   Embla? I hope you are doing
+                   good          """
+
+print(sentence)
+
+
+#Exercise-4
+
+sentence2 = "How are you dear\
+ I hope you are doing good"
+
+print(sentence2)
